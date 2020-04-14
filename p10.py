4ba39fb5e17de967d1b1571ec1591dcb92df4845
Mauriciods07 <54260787+Mauriciods07@users.noreply.github.com>
2020-04-14 14:58:24 -0500

    ejercicio

--- p10.py
+++ p10.py
@@ -136,14 +136,14 @@ print(x)
 import mathplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-x = linspace(0, 5, 20)
+x = linspace(0, 10, 10)
 
 fig, ax = plt.subplots(facecolor='w', edgecolor='k')
-ax.plot(x, sin(x), marker='o', color='r', linestyle='None')
+ax.plot(x, sin(x), marker='*', color='b', linestyle='None')
 
 ax.grid(True)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
+ax.set_xlabel('Eje X')
+ax.set_ylabel('Eje Y')
 ax.grid(True)
 ax.legend(["y = x**2"])
 
